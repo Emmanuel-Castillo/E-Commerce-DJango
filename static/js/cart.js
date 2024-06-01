4371fdba461e2efdf8d198fec4e291068d17be5c
Emmanuel <emmanuel.2001alonso.ec@gmail.com>
2024-06-01 13:04:01 -0500

    Guest Checkout completed using cookies

--- static/js/cart.js
+++ static/js/cart.js
@@ -9,11 +9,37 @@ for (var i = 0; i < updateBtns.length; i++) {
     //check if user is autenticated
     console.log("USER: ", user);
     if (user === "AnonymousUser") {
-      console.log("Not logged in");
+      addCookieItem(productId, action)
     } else {
       updateUserOrder(productId, action)
     }
-  });
+  })
+}
+
+function addCookieItem(productId, action) {
+  console.log("Not logged in here");
+
+  if(action == 'add') {
+    if(cart[productId] == undefined){
+      cart[productId] = {'quantity':1}
+    }
+    else {
+      cart[productId]['quantity'] += 1
+    }
+  }
+
+  if(action == 'remove') {
+      cart[productId]['quantity'] -= 1
+
+      if(cart[productId]['quantity'] <= 0){
+        console.log('Remove Item')
+        delete cart[productId]
+      }
+  }
+
+  console.log('Cart:', cart)
+  document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+  location.reload()
 }
 
 function updateUserOrder(productId, action){
--- store/views.py
+++ store/views.py
@@ -6,48 +6,33 @@ import json
 
 import datetime
 
+from .utils import cookieCart, cartData, guestOrder
+
 # Create your views here.
 def store(request):
 
-  if request.user.is_authenticated:
-    customer = request.user.customer
-    order , created = Order.objects.get_or_create(customer=customer, complete=False)
-    items = order.orderitem_set.all()
-    cartItems = order.get_cart_items
-  else:
-    items = []
-    order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
-    cartItems = order['get_cart_items']
+  data = cartData(request)
+  cartItems = data['cartItems']
 
   products = Product.objects.all()
   context = {'products':products, 'cartItems': cartItems}
   return render(request, 'store/store.html', context)
 
 def cart(request):
-  # check for user autenticated
-  if request.user.is_authenticated:
-    customer = request.user.customer
-    order , created = Order.objects.get_or_create(customer=customer, complete=False)
-    items = order.orderitem_set.all()
-    cartItems = order.get_cart_items
-  else:
-    items = []
-    order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
-    cartItems = order['get_cart_items']
+  data = cartData(request)
+  cartItems = data['cartItems']
+  order = data['order']
+  items = data['items']
 
   context = {'items':items, 'order': order, 'cartItems': cartItems}
   return render(request, 'store/cart.html', context)
 
 def checkout(request):
-  if request.user.is_authenticated:
-    customer = request.user.customer
-    order , created = Order.objects.get_or_create(customer=customer, complete=False)
-    items = order.orderitem_set.all()
-    cartItems = order.get_cart_items
-  else:
-    items = []
-    order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
-    cartItems = order['get_cart_items']
+  
+  data = cartData(request)
+  cartItems = data['cartItems']
+  order = data['order']
+  items = data['items']
 
   context = {'items':items, 'order': order, 'cartItems': cartItems}
   return render(request, 'store/checkout.html', context)
@@ -87,14 +72,18 @@ def processOrder(request):
   if request.user.is_authenticated:
     customer = request.user.customer
     order, created = Order.objects.get_or_create(customer=customer, complete=False)
-    total = float(data['form']['total'])
-    order.transaction_id = transaction_id
 
-    if total == order.get_cart_total:
-      order.complete = True
-    order.save()
+  else:
+    customer, order = guestOrder(request, data)
 
-    if order.shipping == True:
+  total = float(data['form']['total'])
+  order.transaction_id = transaction_id
+
+  if total == order.get_cart_total:
+    order.complete = True
+  order.save()
+
+  if order.shipping == True:
       ShippingAddress.objects.create(
         customer = customer,
         order = order,
@@ -103,7 +92,5 @@ def processOrder(request):
         state = data['shipping']['state'],
         zipcode = data['shipping']['zipcode']
       )
-
-  else:
-    print("User is not logged in")
+      
   return JsonResponse('Payment complete', safe=False)
